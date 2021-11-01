a2a88305091f41d50b6c1d8fb3a1735421ed7456
mr. whalen <michael.whalen@sitechhs.com>
2021-11-01 09:28:04 -0400

    sdfsdf

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,8 +1,7 @@
 import "../styles/style.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
-AOS.init();
+import { menu } from "./menu";
 document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1>Hello?????</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
+console.log(menu);
